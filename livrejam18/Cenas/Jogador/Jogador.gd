8aba55e12c0b91ec71466a6ead01875e6b6ea547
Gabriel de Freitas Garcia <gabrielfreitasgarcia@globo.com>
2018-10-20 15:21:49 -0300

    Alguns testes, trocamos a forma do Collision Shape

--- livrejam18/Cenas/Jogador/Jogador.gd
+++ livrejam18/Cenas/Jogador/Jogador.gd
@@ -1,36 +1,36 @@
 extends KinematicBody2D
 
-const rapidez = 256
-const normal = Vector2(0,-1)
+const rapidez = 256 #Constante de velocidade
+const normal = Vector2(0,-1) #Vetor normal
 
 
-var velocidade = Vector2(0,0)
-var chaves = 3
-var claro = false
-var sprite
+var velocidade #Velocidade linear
+var chaves = 3 #Chaves que o jogador possui
+var claro = false #Se o jogador está, ou não numa área iluminada
+var sprite #Sprite
 
 func _ready():
-	sprite = get_node("AnimatedSprite")
-	set_fixed_process(true)
+	sprite = get_node("AnimatedSprite") #Pega a animação
+	set_fixed_process(true) #Começa o processo físico
 	pass
 
 func _fixed_process(delta):
-	velocidade = Vector2(0,0)
+	velocidade = Vector2(0,0) #Reinicia a velocidade a cada repetição
 	if claro == true:
-		sprite.play("Claro")
+		sprite.play("Claro") #Se está no claro, chama a sprite clara
 	else:
-		sprite.play("Escuro")
+		sprite.play("Escuro") #Se está escuro, chama a sprite escura
 	if Input.is_action_pressed("ui_right"):
-		velocidade.x = 1
+		velocidade.x = 1 #Anda pra direita
 		pass
 	if Input.is_action_pressed("ui_left"):
-		velocidade.x = -1
+		velocidade.x = -1 #Anda pra esquerda
 		pass
 	if Input.is_action_pressed("ui_up"):
-		velocidade.y = -1
+		velocidade.y = -1 #Anda pra cima
 		pass
 	if Input.is_action_pressed("ui_down"):
-		velocidade.y = 1
+		velocidade.y = 1 #Anda pra baixo
 		pass
-	move_and_slide(velocidade.normalized()*rapidez,normal)
+	move_and_slide(velocidade.normalized()*rapidez,normal) #Se move usando um vetor normalizado multiplicado
 	pass
